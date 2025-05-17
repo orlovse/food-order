c53134892ab744be90f69d9d4e1c45e8c9780387
Sergei Orlov <hajoff@gmail.com>
2025-05-17 17:58:58 +0300

    Render restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import { restaurants } from './mock/index.js';
 
+
 function App() {
     return (
-        <ul>
+        <div className="restaurants-list">
 	        {restaurants.map(({ id, name, menu, reviews }) => (
-				<li key={id}>
-					{name}
-				</li>
+				<div
+					key={id}
+					className="restaurant-card"
+				>
+					<h2>
+						{name}
+					</h2>
+					
+					<div>
+						<h3>Menu</h3>
+						
+						<ul>
+							{menu.map(({ id, name }) => (
+								<li key={id}>
+									{name}
+								</li>
+							))}
+						</ul>
+					</div>
+					
+					<div>
+						<h3>Reviews</h3>
+						
+						<ul>
+							{reviews.map(({ id, text }) => (
+								<li key={id}>
+									{text}
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
 	        ))}
-        </ul>
+        </div>
     );
 }
 
