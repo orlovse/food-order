c6a96274708f68cec9ba5a5ca6d339412c4a93e7
Sergei Orlov <hajoff@gmail.com>
2025-05-17 11:06:09 +0300

    Add list of restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { restaurants } from './mock/index.js';
+
 function App() {
-  return ();
+    return (
+        <ul>
+	        {restaurants.map(({ id, name, menu, reviews }) => (
+				<li key={id}>
+					{name}
+				</li>
+	        ))}
+        </ul>
+    );
 }
 
 export default App;
