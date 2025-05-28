49b155ddde8b5875f900ce809f23b8a00377f45a
Orlov S <hajoff@gmail.com>
2025-05-28 20:42:49 +0300

    Add progress bar

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,13 +1,19 @@
+import { useRef } from 'react';
 import { Footer } from '@layout/Footer/Footer';
 import { Header } from '@layout/Header/Header';
+import { ProgressBar } from '@layout/ProgressBar/ProgressBar';
 import styles from './Layout.module.css';
 
 export const Layout = ({ children }) => {
+  const mainRef = useRef(null);
+  
+
   return (
     <div className={styles.layout}>
       <Header />
+      <ProgressBar target={mainRef}/>
 
-      <main>{children}</main>
+      <main ref={mainRef}>{children}</main>
 
       <Footer />
     </div>
