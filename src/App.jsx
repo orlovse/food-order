6be3095aaa2e40073f9fcd902db24373033f7983
Orlov S <hajoff@gmail.com>
2025-06-07 12:50:00 +0300

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { HomePage } from '@pages/HomePage/HomePage';
 import { store } from '@store/store';
 import { AuthContextProvider } from './context/Auth/AuthContextProvider';
 import { ThemeContextProvider } from './context/Theme/ThemeContextProvider';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 export const App = () => {
   return (
     <Provider store={store}>
       <AuthContextProvider>
         <ThemeContextProvider>
+          <BrowserRouter>
           <Layout>
-            <HomePage/>
+            <Routes>
+              <Route path='/' element={<HomePage/>}/>
+            </Routes>
           </Layout>
+          </BrowserRouter>
         </ThemeContextProvider>
       </AuthContextProvider>
     </Provider>
-
   );
 };
